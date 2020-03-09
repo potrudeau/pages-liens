f0cd6e19a6b8b50c83c5acaa79c493cccc3bcb3b
Paul-Olivier Trudeau <po.trudeau@gmail.com>
2020-03-09 09:44:21 -0400

    Ajout de meilleurs logs

--- js/ip.js
+++ js/ip.js
@@ -1,7 +1,7 @@
 var currentIP = "";
 var currentMaison = "";
 
-function redirectOnContext(urlMaison, urlExt)
+function redirectOnContext(linkName, urlMaison, urlExt)
 {
     var redirectUrl = "";
     if (currentIP == currentMaison)
@@ -9,36 +9,38 @@ function redirectOnContext(urlMaison, urlExt)
     else    
         redirectUrl = urlExt;
     
-    console.log(redirectUrl);
+    console.log(linkName + ": " + redirectUrl);
     window.location.replace(redirectUrl);
 }
 
 $( document ).ready(function() {
     
-    $.getJSON('http://ip-api.com/json/', function(data){
+    $.getJSON('http://ip-api.com/json/', function(data){        
         currentIP = data.query;        
+        console.log("Current IP : " + currentIP);
     });    
     $.getJSON('http://ip-api.com/json/maison.p-o.ca', function(data){
-        currentMaison = data.query;          
+        currentMaison = data.query;    
+        console.log("Maison IP : " + currentMaison);      
     });
     
     $("#diskstation").click(function(){
-        redirectOnContext("https://10.0.0.2:5001/", "https://maison.p-o.ca:5001/");        
+        redirectOnContext("diskstation", "https://10.0.0.2:5001/", "https://maison.p-o.ca:5001/");        
     });    
 
     $("#photostation").click(function(){
-        redirectOnContext("https://10.0.0.2/photo/", "https://maison.p-o.ca:8081/photo/");        
+        redirectOnContext("photostation", "https://10.0.0.2/photo/", "https://maison.p-o.ca:8081/photo/");        
     });            
 
     $("#videostation").click(function(){
-        redirectOnContext("https://10.0.0.2:5001/?launchApp=SYNO.SDS.VideoStation.AppInstance", "https://maison.p-o.ca:5001/?launchApp=SYNO.SDS.VideoStation.AppInstance");        
+        redirectOnContext("videostation", "https://10.0.0.2:5001/?launchApp=SYNO.SDS.VideoStation.AppInstance", "https://maison.p-o.ca:5001/?launchApp=SYNO.SDS.VideoStation.AppInstance");        
     });        
 
     $("#audiostation").click(function(){
-        redirectOnContext("https://10.0.0.2:5001/?launchApp=SYNO.SDS.AudioStation.Application", "https://maison.p-o.ca:5001/?launchApp=SYNO.SDS.AudioStation.Application");        
+        redirectOnContext("audiostation","https://10.0.0.2:5001/?launchApp=SYNO.SDS.AudioStation.Application", "https://maison.p-o.ca:5001/?launchApp=SYNO.SDS.AudioStation.Application");        
     });      
     
     $("#homebridge").click(function(){
-        redirectOnContext("https://maison.p-o.ca:8581", "https://maison.p-o.ca:8581");        
+        redirectOnContext("homebridge","https://maison.p-o.ca:8581", "https://maison.p-o.ca:8581");        
     });       
 });
